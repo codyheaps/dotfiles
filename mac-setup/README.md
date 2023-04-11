b88bbaecda220b46a93a1de16d65d9adf65e60ef
codyheaps <cody.heaps@loanpro.io>
2023-04-11 12:03:39 -0600

    mac-setup

---
+++ mac-setup/README.md
@@ -0,0 +1,4 @@
+TODO: Automate this stuff
+```git --version```
+```sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"```
+```/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"```
---
+++ mac-setup/makesymlinks.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+files=".alias .vimrc .zshrc .p10k.zsh"
+
+echo "Creating ~/dotfiles_old for backup of existing dotfiles..."
+mkdir -p ~/dotfiles_old
+
+echo "Changing to ~/dotfiles..."
+cd ~/dotfiles
+
+# relocate existing dotfiles to dotfiles_old, then create symlink from ~ files to $files
+echo "Relocating existing dotfiles to ~/dotfiles_old..."
+for file in $files;
+do
+    mv ~/$file ~/dotfiles_old
+    echo "Creating symlink for $file..."
+    ln -s ~/dotfiles/$file ~/$file
+done
+
