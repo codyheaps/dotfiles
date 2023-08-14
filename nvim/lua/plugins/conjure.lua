302f418d32cbfc7847bb31682dbe4f935aba18c4
Cody Heaps <cody.heaps@loanpro.io>
2023-08-14 09:03:44 -0600

    add conjure to neovim

---
+++ nvim/lua/plugins/conjure.lua
@@ -0,0 +1,36 @@
+return {
+    {
+        "tpope/vim-dispatch",
+        "clojure-vim/vim-jack-in",
+        "radenling/vim-dispatch-neovim",
+        "Olical/conjure",
+        ft = { "clojure", "fennel", "python" },
+        dependencies = {
+            {
+                "PaterJason/cmp-conjure",
+                config = function()
+                    local cmp = require("cmp")
+                    local config = cmp.get_config()
+                    table.insert(config.sources, {
+                        name = "buffer",
+                        option = {
+                            sources = {
+                                {name = "conjure"}
+                            }
+                        }
+                    })
+                    cmp.setup(config)
+                end
+            }
+        },
+        config = function(_, opts)
+            require("conjure.main").main()
+            require("conjure.mapping")["on-filetype"]()
+        end,
+        init = function()
+            -- Set configuration options here
+            vim.g["conjure#debug"] = true
+            vim.g["conjure#log#hud#enabled"] = false
+        end
+    }
+}
